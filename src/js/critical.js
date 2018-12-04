5c9aa8f3cdd1caeddfd7be64a503930038f2fb26
Jan Diehm <jandiehm@Jans-MBP-2.lan1>
2018-12-04 10:22:30 -0600

    adds fonts

--- src/js/critical.js
+++ src/js/critical.js
@@ -6,14 +6,15 @@ import './polyfills/find';
 import './polyfills/includes';
 import { loadFontGroup } from './utils/load-font';
 
-const canela = [
-	{ family: 'Canela Web', weight: 300 },
-	{ family: 'Canela Web', weight: 700 }
+const national = [
+	{ family: 'National 2 Narrow Web', weight: 200 },
+	{ family: 'National 2 Narrow Web', weight: 700 },
+	{ family: 'National 2 Narrow Web', weight: 900 }
 ];
 
-const publico = [
-	{ family: 'Publico Text Web', weight: 400 },
-	{ family: 'Publico Text Web', weight: 700 }
+const tiempos = [
+	{ family: 'Tiempos Text Web', weight: 500 },
+	{ family: 'Tiempos Text Web', weight: 700 }
 ];
 
 const atlas = [
@@ -26,6 +27,6 @@ const atlas = [
 if (!window.Promise) window.Promise = Promise;
 
 // load fonts
-loadFontGroup(canela);
-loadFontGroup(publico);
+loadFontGroup(national);
+loadFontGroup(tiempos);
 loadFontGroup(atlas);
